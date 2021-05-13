8aed7571790323b8ab2112efef690b83b6ec90c8
MBeckett <maddbeck@gmail.com>
2021-05-13 14:47:36 -0500

    Completed Support Classes (Location, CoreCompetency, and  PositionType)

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,21 @@
+package org.launchcode.techjobs_oo.Tests;
+import org.launchcode.techjobs_oo.Job;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JobTest {
+
+    @Before
+    public void createJobObjects() {
+        Job jobObject1 = new Job();
+        Job jobObject2 = new Job();
+    }
+
+
+    @Test
+    public void testDifferentIdValues(){
+
+    }
+
+}
